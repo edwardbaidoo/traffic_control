ba648a62e7eb71fa8baa67342501c85e63f094e2
edwardbaidoo <edwardjacobbaidoo@gmail.com>
2019-08-14 13:00:37 +0000

    first commit

---
+++ traffic.py
@@ -0,0 +1,17 @@
+from gpiozero import LED
+
+#### NOTES on GPIO PINS
+# Number of Traffic Heads = 2
+# Number of Leds per head = 3
+# Pins per head = 4(3 GPIO, 1 Ground)
+# Ground = Pin6/39
+
+#### HEAD 1 details
+# LED1 = h1l1 = 15(22)
+# LED2 = h1l2 = 16(23)
+# LED3 = h1l3 = 18(24)
+
+#### HEAD 2 Details
+# LED 1 = h2l1 = 38(20)
+# LED 2 = h2l2 = 40(21)
+# LED 3 = h2l3 = 37(26)
