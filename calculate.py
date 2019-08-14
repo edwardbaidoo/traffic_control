428166a23044d632a7b9cec635bc46764834f10c
edwardbaidoo <edwardjacobbaidoo@gmail.com>
2019-08-14 23:10:38 +0000

    calc module added

---
+++ calculate.py
@@ -0,0 +1,14 @@
+#Guideline for vatiables
+#design capacity - d
+#Minimum input - x
+#maximum input - x
+#represent as percent value - v
+#minimum norm range - a
+#maximum norm range - b
+#norm value - y - (-1 >= y <= 1)
+
+# Formula used 
+#   v=(d-x)/d*100
+#   
+#
+#
